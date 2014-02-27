6fc72526508122a83800d65d156e06d28a477cb4
Kenji Ono <keno@riken.jp>
2014-02-27 13:53:04 +0900

    1.6.2

--- install_intel.sh
+++ install_intel.sh
@@ -29,13 +29,13 @@ export TMP_F90=mpif90
 
 
 # library name
-export TP_LIB=TextParser-1.4.6
+export TP_LIB=TextParser-1.4.8
 export PM_LIB=PMlib-2.0.0
-export PLY_LIB=Polylib-2.7.4
-export CUT_LIB=Cutlib-3.2.0
+export PLY_LIB=Polylib-3.3.2
+export CUT_LIB=Cutlib-3.2.1
 export CPM_LIB=CPMlib-1.1.6
-export CIO_LIB=CIOlib-1.5.3
-export FFVC=FFVC-1.6.0
+export CIO_LIB=CIOlib-1.5.4
+export FFVC=FFVC-1.6.2
 
 # TextParser
 #
