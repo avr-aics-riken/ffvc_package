30ef5d1b32302f5ff9ba0c7bdf45da58ac4ef51e
Kenji Ono <keno@riken.jp>
2014-08-01 07:18:58 +0900

    1.8.1

--- install_intel.sh
+++ install_intel.sh
@@ -29,13 +29,13 @@ export TMP_F90=mpif90
 
 
 # library name
-export TP_LIB=TextParser-1.5.1
-export PM_LIB=PMlib-2.0.2
-export PLY_LIB=Polylib-3.4.4
-export CUT_LIB=Cutlib-3.2.3
-export CPM_LIB=CPMlib-1.2.1
-export CIO_LIB=CIOlib-1.5.5
-export FFVC=FFVC-1.8.0
+export TP_LIB=TextParser-1.5.6
+export PM_LIB=PMlib-2.1.4
+export PLY_LIB=Polylib-3.4.7
+export CUT_LIB=Cutlib-3.2.5
+export CPM_LIB=CPMlib-1.2.2
+export CIO_LIB=CIOlib-1.5.8
+export FFVC=FFVC-1.8.1
 
 # TextParser
 #
